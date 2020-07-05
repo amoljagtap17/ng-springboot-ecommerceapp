b7640485395e30bc5ba5502b81229853ac388001
Amol Jagtap <amoljagtap17@gmail.com>
2020-07-05 20:47:35 +0530

    NG-SPRINGBOOT-ECOMMERCE-APP: Fetch products data from API and display in product-list component

--- backend/ng-springboot-ecommerceapp/src/main/java/com/ngspringboot/ecommerceapp/dao/ProductRepository.java
+++ backend/ng-springboot-ecommerceapp/src/main/java/com/ngspringboot/ecommerceapp/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.ngspringboot.ecommerceapp.dao;
 
 import com.ngspringboot.ecommerceapp.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
