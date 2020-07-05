fa4baae55990597df629bd37615b3e06c155523d
Amol Jagtap <amoljagtap17@gmail.com>
2020-07-05 15:56:38 +0530

    NG-SPRINGBOOT-ECOMMERCE-APP: Sprint Boot starter kit

---
+++ backend/ng-springboot-ecommerceapp/src/main/java/com/ngspringboot/ecommerceapp/NgSpringbootEcommerceappApplication.java
@@ -0,0 +1,13 @@
+package com.ngspringboot.ecommerceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgSpringbootEcommerceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgSpringbootEcommerceappApplication.class, args);
+	}
+
+}
---
+++ backend/ng-springboot-ecommerceapp/src/test/java/com/ngspringboot/ecommerceapp/NgSpringbootEcommerceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngspringboot.ecommerceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgSpringbootEcommerceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
