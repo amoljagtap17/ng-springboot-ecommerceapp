0eaf36c2769cee31cb7e77fd04278d24747da8e6
Amol Jagtap <amoljagtap17@gmail.com>
2020-07-05 16:25:31 +0530

    NG-SPRINGBOOT-ECOMMERCE-APP: Add repository interfaces

---
+++ backend/ng-springboot-ecommerceapp/src/main/java/com/ngspringboot/ecommerceapp/dao/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.ngspringboot.ecommerceapp.dao;
+
+import com.ngspringboot.ecommerceapp.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
