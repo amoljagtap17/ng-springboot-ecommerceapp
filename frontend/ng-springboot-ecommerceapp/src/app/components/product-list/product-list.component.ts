4276b9c8fc9079fc94378391bc6c3aa33eb436aa
Amol Jagtap <amoljagtap17@gmail.com>
2020-07-06 21:28:55 +0530

    NG-SPRINGBOOT-ECOMMERCE-APP: Pass route params to getProductList function

--- frontend/ng-springboot-ecommerceapp/src/app/components/product-list/product-list.component.ts
+++ frontend/ng-springboot-ecommerceapp/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
@@ -10,17 +11,33 @@ import { Product } from 'src/app/common/product';
 })
 export class ProductListComponent implements OnInit {
   products: Product[];
+  currentCategoryId: number;
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   // tslint:disable-next-line: typedef
   listProducts() {
-    this.productService.getProductList().subscribe((data) => {
-      this.products = data;
-    });
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    } else {
+      this.currentCategoryId = 1;
+    }
+
+    this.productService
+      .getProductList(this.currentCategoryId)
+      .subscribe((data) => {
+        this.products = data;
+      });
   }
 }
